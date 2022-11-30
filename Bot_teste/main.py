d54d9c4ea495ba8eb8de276bc2de038a749e2d91
Leonardo Gonçalves Machado <leonardogm2003@gmail.com>
2022-11-30 13:23:50 -0300

    colocando o menu de comandos issue #14

--- Bot_teste/main.py
+++ Bot_teste/main.py
@@ -1,16 +1,21 @@
 from telegram.ext import *
 from telegram import *
 from teacher import TeacherText
+import emoji
 
-TOKEN = "5840991803:AAGxIhCYY3dNqpQglvaxkdLZp0zMAwKlpZk"
+print("Bot started...")
+
+TOKEN = ""
 
 ##comando iniciais
 teacherText = "Professor"
 studentText = "Aluno"
 
 def start(update, context):
-    buttons = [[KeyboardButton(teacherText)], [KeyboardButton(studentText)]]
-    context.bot.send_message(chat_id=update.effective_chat.id, text = "Olá, seja bem vindo ao Boto! \nPrimeiro gostariamos de algumas informações.", reply_markup=ReplyKeyboardMarkup(buttons))
+    buttons = [[KeyboardButton(teacherText)], [KeyboardButton(studentText)], [KeyboardButton("Menu de Comandos")]]
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text = "Olá, seja bem vindo ao Boto! \nPrimeiro gostariamos de algumas informações.",
+                             reply_markup=ReplyKeyboardMarkup(buttons))
 
 def handle_message(update, context):
     if teacherText in update.message.text:
@@ -24,12 +29,44 @@ def handle_message(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text = "Olá, Aluno",
                                  reply_markup=ReplyKeyboardRemove())
 
+    if "Menu de Comandos" in update.message.text:
+        update.message.reply_text("Eu posso te ajudar a enviar e acessar conteúdos e materiais.\n "
+                                  "Você pode utilizar os seguintes comandos:\n"
+                                  "\n"
+                                  "/novoConteudo - envia um link de um novo conteúdo para a base de dados;\n"
+                                  "/acessarConteudo - acessa um conteúdo existente na base de dados;\n"
+                                  "/deletarConteudo - apaga um conteudo na base de dados;\n"
+                                  "/editarConteudo - altera um conteudo existente na base de dados.")
+
+
+## comando para quando alguma funcao nao esta pronta
+def not_finished(update, context):
+    update.message.reply_text("Ainda estamos trabalhando nesse comando." + emoji.emojize(':hammer_and_wrench:'))
+
+##menu de comandos
+def help_command(update, context):
+
+    update.message.reply_text("Eu posso te ajudar a enviar e acessar conteúdos e materiais.\n "
+                              "Você pode utilizar os seguintes comandos:\n"
+                              "\n"
+                              "/novoConteudo - envia um link de um novo conteúdo para a base de dados;\n"
+                              "/acessarConteudo - acessa um conteúdo existente na base de dados;\n"
+                              "/deletarConteudo - apaga um conteudo na base de dados;\n"
+                              "/editarConteudo - altera um conteudo existente na base de dados.")
+
+
 def main():
 
     updater = Updater(token=TOKEN)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_command))
+
+    dp.add_handler(CommandHandler("novoConteudo", not_finished))
+    dp.add_handler(CommandHandler("acessarConteudo", not_finished))
+    dp.add_handler(CommandHandler("deletarConteudo", not_finished))
+    dp.add_handler(CommandHandler("editarConteudo", not_finished))
 
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
